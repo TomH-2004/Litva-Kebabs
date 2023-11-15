f4da4c59173c08716395e6a25b8f9a4a8eea6dc6
TomH-2004 <tom@datalords.net>
2023-11-15 09:41:09 +0000

    .

--- go/main.go
+++ go/main.go
@@ -65,7 +65,7 @@ func main() {
 	r.Handle("/updateprofile", authMiddleware(http.HandlerFunc(updateProfileHandler))).Methods("POST")
 	r.Handle("/signoutafterupdate", authMiddleware(http.HandlerFunc(signoutHandlerAfterUpdate))).Methods("GET")
 
-	//PGIN AND SIGN UP
+	//LOGIN AND SIGN UP
 	r.HandleFunc("/", loginHandler).Methods("GET")
 	r.HandleFunc("/", loginPostHandler).Methods("POST")
 	r.HandleFunc("/signup", signupHandler).Methods("POST")
